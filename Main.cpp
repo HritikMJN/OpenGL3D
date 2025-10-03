d81eb7d6fdfaa537bee6c1aef17281ac5ae6af21
Hritik Mahajan <hritik.mahajan@gmail.com>
2025-10-03 19:16:14 +0530

    OpenGL 3D 2025 Project

---
+++ Main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(400, 225, "HritikMahajan", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+
+	glViewport(0, 0, 400, 225);
+
+	glClearColor(0.85f, 0.7f, 0.25f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
